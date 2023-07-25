1802cfa3ccbff717d772e0d17284441a5f33ac89
selfridicule <selfridicule@gmail.com>
2023-07-25 16:15:49 +0800

    描述：：下拉加载数据

---
+++ pages/request/request.js
@@ -0,0 +1,91 @@
+// pages/request/request.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      catList : []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.getData()
+  },
+
+  getData(){
+    wx.request({
+      url: 'https://api.thecatapi.com/v1/images/search?limit=2',
+      method : "get",
+      success : res=>{
+        this.data.catList = [];
+        console.log('getData()' , res);
+        let data =  res.data;
+        for (let i = 0; i < data.length; i++) {
+          this.data.catList.push({
+            id : data[i].id,
+            url : data[i].url,
+            width : data[i].width,
+            height : data[i].height,
+          })
+        }
+        this.setData({
+          catList : this.data.catList
+        })
+        wx.stopPullDownRefresh();
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    console.log(111);
+    this.getData()
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
